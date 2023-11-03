7e4a4ecbc334f9ed41b73d71d10e37179c910321
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-03 09:18:40 +0600

    Basic data types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,38 @@
+// basic data type
+
+// string
+
+let firstName: string = "Mezba";
+
+// number
+let roll: number = 123;
+
+// bolliean
+let isAdmint: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+
+let d:number;
+
+d = 123;
+// d = '123sd';
+// d = 'true';
+
+
+// non primitive
+// Array
+
+let friends: string[] = ['rachel', 'monica'];
+
+let rollList: number[] = [14, 4, 2];
+
+// tuple --<>array -->order-->type of values
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName:[number,string,boolean] = [50, 'mr.DX',true];
